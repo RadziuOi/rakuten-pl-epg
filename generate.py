1ec8e4c7b5f5d167c577aeb14b8545b8d8a56e24
RadziuOi <radziu.zwc@gmail.com>
2026-09-22 10:02:08 +0200

    Update User-Agent and Accept-Language headers

--- generate.py
+++ generate.py
@@ -88,9 +88,11 @@ base_params = {
     "per_page": "50"
 }
 
+# Zaktualizowane nagłówki imitujące rzeczywiste zapytanie z przeglądarki
 headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
     "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
     "Origin": "https://rakuten.tv",
     "Referer": "https://rakuten.tv/"
 }
